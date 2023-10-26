df131932bd3287c273177ad51d1a255418603b2c
LeandroPura08 <thanoy08@gmail.com>
2023-10-27 04:53:38 +0800

    upload

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+document.querySelector('.show-tran').addEventListener('click',()=>{
+
+})
+
+
+document.querySelector('.show-pauts').addEventListener('click',()=>{
+  
+})
+
+document.querySelector('.create-tran').addEventListener('click',()=>{
+  document.querySelector('.main').classList.add("create-tran-selected");
+  document.querySelector('.create-tran-selected').innerHTML=
+  `
+
+  `
+})
